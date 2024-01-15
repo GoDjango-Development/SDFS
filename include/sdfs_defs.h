5793261194f229fc58d49a94c702a3cb2271db73
user <lmdelbahia@gmail.com>
2024-01-15 15:10:17 -0500

    feat/fix: add datatypes. change error numbers.

--- include/sdfs_defs.h
+++ include/sdfs_defs.h
@@ -1,9 +1,11 @@
 #ifndef SDFS_DEFS_H
 #define SDFS_DEFS_H
 
+#include <inttypes.h>
+
 /* global error definitions */
 #define SDFS_SUCCESS 0
-#define SDFS_ERROR 1
+#define SDFS_ERROR -1
 
 /* global error messages definitions */
 #define SDFS_SUCCESSMSG "sdfs: operation completed"
@@ -12,5 +14,7 @@
 /* sdfs datatypes */
 typedef char *sdfs_str;
 typedef int sdfs_err;
+typedef int64_t sdfs_int64;
+typedef void *sdfs_buf;
 
 #endif
