f2e9e00c84721a2a8a33a08fd38e10289c9be3e2
user <lmdelbahia@gmail.com>
2024-02-02 12:17:54 -0500

    feat: refactoring.

--- include/sdfs_seclayer.h
+++ include/sdfs_seclayer.h
@@ -1,3 +1,6 @@
+/* programmer: luis miguel
+ * email: lmdelbahia@gmail.com */
+
 #ifndef SECLAYER_H
 #define SECLAYER_H
 
@@ -11,6 +14,7 @@
 #define SDFS_SECERROR SDFS_ERROR + -1
 #define SDFS_SECEMEM SDFS_ERROR + -2
 #define SDFS_SECEUSR SDFS_ERROR + -3
+#define SDFS_SECELOGIN SDFS_ERROR + -4
 
 /* security layer specific datatypes */
 
@@ -23,6 +27,8 @@ typedef struct sdfs_id *sdfs_id;
 sdfs_err sdfs_secinit(sdfs_id *id, sdfs_str usrfile);
 /* login function */
 sdfs_err sdfs_seclogin(sdfs_id id, sdfs_str usr, sdfs_str pwd);
+/* logout function */
+void sdfs_seclogout(sdfs_id id);
 /* finalize security layer */
 void sdfs_secfin(sdfs_id id);
 
--- src/sdfs_seclayer.c
+++ src/sdfs_seclayer.c
@@ -1,6 +1,13 @@
+/* programmer: luis miguel
+ * email: lmdelbahia@gmail.com */
+
 #include <sdfs_seclayer.h>
+#include <sdfs_util.h>
 #include <malloc.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define SC_INVUSR -1
 
@@ -12,6 +19,9 @@ struct sdfs_id{
     sdfs_uid id;
     sdfs_char inv;
 };
+
+/* login recheck */
+static sdfs_err sdfs_lrcheck(sdfs_id id);
 
 /* initialize security layer */
 sdfs_err sdfs_secinit(sdfs_id *id, sdfs_str usrfile)
@@ -30,11 +40,43 @@ sdfs_err sdfs_seclogin(sdfs_id id, sdfs_str usr, sdfs_str pwd)
 {
     strcpy(id->usr, usr);
     strcpy(id->pwd, pwd);
-    return SDFS_SECSUCCESS;
+    return sdfs_lrcheck(id);
+}
+
+/* logout function */
+void sdfs_seclogout(sdfs_id id)
+{
+    id->id = SC_INVUSR;
 }
 
 /* finalize security layer */
 void sdfs_secfin(sdfs_id id)
 {
     free(id);
+}
+
+/* login recheck */
+static sdfs_err sdfs_lrcheck(sdfs_id id)
+{
+    int fd = open(id->usrfile, O_RDONLY);
+    if (fd == -1)
+        return SDFS_SECELOGIN;
+    struct stat st;
+    if (stat(id->usrfile, &st) == -1) {
+        close(fd);
+        return SDFS_SECELOGIN;
+    }
+    void *buf = malloc(st.st_size);
+    if (!buf) {
+        close(fd);
+        return SDFS_SECEMEM;
+    }
+    if (sdfs_readchunk(fd, buf, st.st_size) == -1) {
+        close(fd);
+        free(buf);
+        return SDFS_SECELOGIN;
+    }
+    close(fd);
+    free(buf);
+    return SDFS_SECSUCCESS;
 }
